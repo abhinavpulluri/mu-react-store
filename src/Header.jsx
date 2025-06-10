99938d3835985a835ca48b87edb9a472a4a5bb81
abhinavpulluri <abhinavpulluri20@gmail.com>
2025-06-10 14:18:58 +0530

    Updated cart

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,6 +10,7 @@ export default function Header({name}) {
             <ul>
                 <li><Link to="/"> Home</Link></li>
                 <li><Link to="/login">Login</Link></li>
+                <li><Link to="/cart">Cart</Link></li>
             </ul>
         </div>
     </div>
