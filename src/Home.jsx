4e18ca15a3a2e6d72277f3dc97cf89e2e80119f8
abhinavpulluri <abhinavpulluri20@gmail.com>
2025-06-14 14:26:57 +0530

    updated the backend api

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import './Home.css'
 import { useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { AppContext } from './App';
+import axios from 'axios';
 
 export default function Home() {
-
     const {cart, setCart, email} = useContext(AppContext);
-    
-    const products = [
-        {id: 1, name: "Product 1", price: 100, desc:"This is the description of product 1",imgURL:"https://picsum.photos/id/1/300/300"},
-        {id: 2, name: "Product 2", price: 200, desc:"This is the description of product 2",imgURL:"https://picsum.photos/id/2/300/300"},
-        {id: 3, name: "Product 3", price: 300, desc:"This is the description of product 3",imgURL:"https://picsum.photos/id/3/300/300"},
-    ];
-
+    const [products, setProducts] = useState([]);
     const Navigate = useNavigate();
+    const API = import.meta.env.VITE_API_URL;
+    
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const url = `${API}/api/products`;
+                const res = await axios.get(url);
+                setProducts(res.data);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+        fetchProducts();
+    }, []);
 
     const buyNow = (obj) => {
         setCart({id: obj.id, name: obj.name, price: obj.price, desc: obj.desc, imgURL: obj.imgURL, qty: 1, email: email});
@@ -23,17 +31,20 @@ export default function Home() {
     
     return (
     <div className='App-Home-Row'>
-        {/* My name is {name}. My age is {age}. */}
-        {products.map((product) => (
-            <div key={product.id}>
-                <img src={product.imgURL} alt={product.name} />
-                <h2>{product.name}</h2>
-                <p>{product.desc}</p>
-                <h4>Price: ₹{product.price}</h4>
-                <p><button onClick={()=>buyNow(product)}>Buy now</button></p>
-                <hr />
-            </div>
-        ))}
+        {products.length === 0 ? (
+            <p>No products found.</p>
+        ) : (
+            products.map((product) => (
+                <div key={product.id}>
+                    <img src={product.imgURL} alt={product.name} />
+                    <h2>{product.name}</h2>
+                    <p>{product.desc}</p>
+                    <h4>Price: ₹{product.price}</h4>
+                    <p><button onClick={()=>buyNow(product)}>Buy now</button></p>
+                    <hr />
+                </div>
+            ))
+        )}
     </div>
   )
 }
